bffe2fbd4b14650240700fd4800c1dcac66e5a8a
Jiawei Jiang <916754699@qq.com>
2023-08-11 12:49:51 +0800

    Update __init__.py

--- opencd/models/necks/__init__.py
+++ opencd/models/necks/__init__.py
@@ -1,4 +1,5 @@
 from .feature_fusion import FeatureFusionNeck
 from .tiny_fpn import TinyFPN
+from .ds_fpn import DS_FPN
 
-__all__ = ['FeatureFusionNeck', 'TinyFPN']
+__all__ = ['FeatureFusionNeck', 'TinyFPN', 'DS_FPN']
