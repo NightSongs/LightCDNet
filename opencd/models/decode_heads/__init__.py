4208f9c5200f78cc8566423a832acf9a23bcaf5f
Jiawei Jiang <916754699@qq.com>
2023-08-11 12:47:49 +0800

    Update __init__.py

--- opencd/models/decode_heads/__init__.py
+++ opencd/models/decode_heads/__init__.py
@@ -5,6 +5,7 @@ from .identity_head import IdentityHead, DSIdentityHead
 from .tiny_head import TinyHead
 from .multi_head import MultiHeadDecoder
 from .general_scd_head import GeneralSCDHead
+from .ds_fpn_head import DS_FPNHead
 
 __all__ = ['BITHead', 'Changer', 'STAHead', 'IdentityHead', 'DSIdentityHead', 
-           'TinyHead', 'MultiHeadDecoder', 'GeneralSCDHead']
+           'TinyHead', 'MultiHeadDecoder', 'GeneralSCDHead', 'DS_FPNHead']
